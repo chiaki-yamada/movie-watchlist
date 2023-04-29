28a6ebb78b6319c26b24fd3758d0a2b9adc70f7a
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-04-28 17:14:29 -0700

    first commit

---
+++ src/components/Add.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import { ResultCard } from './ResultCard';
+
+export const Add = () => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] =useState([]);
+
+  const onChange = (e)=>{
+    setQuery(e.target.value);
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=f76eb148de48f47eb397524d7427844c&language=en-US&page=1&include_adult=false&query=${e.target.value}}`)
+    .then (res => res.json())
+    .then (data => {
+      if (!data.errors) {
+        setResults(data.results);
+      }else {
+        setResults([]);
+      }
+    })
+    
+  
+  };
+
+  return (
+    <div className='add-page'>
+      <div className='container'>
+        <div className='add-content'>
+          <div className='input-wrapper'>
+            <input 
+              type='text'
+              placeholder='Search for a movie'
+              value={query}
+              onChange={onChange}
+             
+            />
+          </div>
+
+          {results.length > 0 && (
+            <ul className='results'>
+              {results.map((movie) =>(
+                <li key={movie.id}>
+                  <ResultCard movie={movie} />
+
+                </li>
+    
+              ))}
+            </ul>
+          )}
+                
+        </div>
+      </div>
+
+    </div>
+  )
+}
